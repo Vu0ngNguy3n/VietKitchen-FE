602747658f27613589494e1596215f0131a7ed78
HOP NGUYEN <hopnvhe164017@fpt.edu.vn>
2024-06-19 17:41:34 +0700

    package and permission admin

--- src/pages/admin/Permissions.jsx
+++ src/pages/admin/Permissions.jsx
@@ -1,25 +1,100 @@
-import Dashboardview from "../../components/adminComponent/DashboardView"
-import Sidebar from "../../components/adminComponent/Sidebar"
+import Dashboardview from "../../components/adminComponent/DashboardView";
+import Sidebar from "../../components/adminComponent/Sidebar";
+import { FaSearch, FaEdit } from "react-icons/fa";
+import { useNavigate } from "react-router";
+import { useState } from "react";
 
+function Permissions() {
+    const [statusTable, setStatusTable] = useState("enable");
+    const navigate = useNavigate();
 
-function Permissions(){
-    return(
+    const listPermission = [
+        {
+            name: "Admin",
+            description: "Tao là to nhất",
+        },
+        {
+            name: "Chủ nhà hàng",
+            description: "Tao là chủ nhà hàng",
+        },
+        {
+            name: "Quản lý nhà hàng",
+            description: "Thằng chủ nó thuê tao về quản lý",
+        },
+        {
+            name: "Nhân viên",
+            description: "Thấp cổ bé họng",
+        },
+    ];
+
+    return (
         <div className="">
-            <div className="flex overflow-scroll ">
+            <div className="flex">
                 <div className="basis-[12%] h-[100vh]">
                     <Sidebar />
                 </div>
                 <div className="basis-[88%] border overflow-scroll h-[100vh]">
                     <Dashboardview />
-                    <div className="min-w-[40rem] round ">
-                        
+                    <div className="min-w-[40]x rounded-lg bg-white p-16 shadow min-h-[90vh] mt-2">
+                        <h1 className="font-black text-3xl">Danh sách quyền</h1>
+
+                        <div className="flex mt-6 flex-col gap-4 md:flex-row justify-between">
+                            <div className="">
+                                <div className="relative grow rounded-md border-2 border-gray-300">
+                                    <FaSearch className="absolute left-3 top-3.5 h-5 w-5 text-gray-400" />
+                                    <input
+                                        type="text"
+                                        className="w-full px-4 py-3 pl-10 outline-none italic "
+                                        placeholder="Nhập tên quyền"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            STT
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Tên Quyền
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Mô Tả
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            <span className="sr-only">Chỉnh sửa</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {listPermission.map((permiss, index) => (
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
+                                            <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {index + 1}
+                                            </td>
+                                            <td className="px-6 py-4">{permiss.name}</td>
+                                            <td className="px-6 py-4">{permiss.description}</td>
+                                            <td className="px-6 py-4 flex justify-center">
+                                                <button
+                                                    className="py-2 px-5 bg-secondary font-semibold text-white rounded hover:bg-primary transition-all duration-300 flex items-center"
+                                                >
+                                                    <FaEdit className="mr-1" />
+                                                    Chỉnh sửa
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
-    )
+    );
 }
 
-export default Permissions
+export default Permissions;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
         "primary": "#010851",
         "secondary": "#9A7AF1",
         "tartiary": "#707070",
-        "pink": "#EE9AE5"
+        "pink": "#EE9AE5",
+        "lgreen":"#4CBB17",
+        "green": "#008000	"
       },
       boxShadow: {
         '3xl': '0px 10px 50px 0px rgba(0, 0, 0, 0.15)',
